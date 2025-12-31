62bf7746dc62a0033ab149ec0ad6a30ef52e83c2
Klemen Slavič <krof.drakula@gmail.com>
2025-12-31 17:24:46 +0100

    Reset all

--- src/utils/contexts.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "preact";
-import type { StateUpdater } from "preact/hooks";
-
-export type Theme = "light" | "dark";
-
-export const ColorTheme = createContext<{
-  theme: Theme | null;
-  setTheme: StateUpdater<Theme | null>;
-}>(null);
